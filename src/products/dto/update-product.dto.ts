9b8595f4f592409834bb822d1cf2f1e13a7aa2b2
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-11 16:53:50 +0400

    feat: add dto for product update

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+  @IsNumber()
+  @IsNotEmpty()
+  categoryId: number;
+  @IsNumber()
+  @IsNotEmpty()
+  stock: number;
+  @IsString()
+  @IsOptional()
+  description: string;
+}
