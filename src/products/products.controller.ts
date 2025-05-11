896fa2108b371f33caaea60d90c358cd8d50491d
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-11 16:53:38 +0400

    feat: add product crud

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
-@Controller('products')
+@Controller('/api/products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
@@ -22,7 +30,7 @@ export class ProductsController {
     return this.productsService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+  create(data: CreateProductDto) {
+    return this.prisma.products.create({
+      data,
+    });
   }
-
   findAll() {
-    return `This action returns all products`;
+    return this.prisma.products.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.prisma.products.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: number, data: UpdateProductDto) {
+    return this.prisma.products.update({
+      where: {
+        id,
+      },
+      data,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    return this.prisma.products.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
