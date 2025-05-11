463975d4a8949760182709097ac584a5ed2b2b63
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-11 16:47:50 +0400

    feat: add validation rules for create product function

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+  @IsNumber()
+  @IsNotEmpty()
+  categoryId: number;
+  @IsNumber()
+  @IsNotEmpty()
+  stock: number;
+  @IsString()
+  @IsOptional()
+  description: string;
+}
